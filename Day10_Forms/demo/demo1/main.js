523f55d2b324d1a44c3cb711ecdb6c32c674b556
SuperCoder <propscode254@gmail.com>
2023-08-22 10:39:26 +0200

    add korrect js

--- Day10_Forms/demo/demo1/main.js
+++ Day10_Forms/demo/demo1/main.js
@@ -1,5 +1,20 @@
-function feeling() {
-  let range = document.querySelector("input[type='range']").value;
-  let output = document.querySelector(".output");
-  output.innerHTML = `ich fühle mich wie ein ${range}`;
-}
+// function feeling() {
+//   let range = document.querySelector("input[type='range']").value;
+//   let output = document.querySelector(".output");
+//   output.innerHTML = `ich fühle mich wie ein ${range}`;
+// }
+// main.js
+
+// Event listener, der ausgelöst wird, wenn sich der Wert des Bereichs ändert
+document.addEventListener("DOMContentLoaded", function () {
+  // Funktion, die den Wert des Bereichs in das .output-Element einfügt
+  function feeling() {
+    let range = document.querySelector("input[type='range']").value;
+    let output = document.querySelector(".output");
+    output.innerHTML = `Ich fühle mich wie eine ${range}`;
+  }
+
+  // Hinzufügen des Event Listeners zum "input" Element vom Typ "range"
+  let rangeInput = document.querySelector("input[type='range']");
+  rangeInput.addEventListener("input", feeling);
+});
