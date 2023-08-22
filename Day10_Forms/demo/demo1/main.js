de4decc433c48fe4f67def75694428dd0eb6bccf
SuperCoder <propscode254@gmail.com>
2023-08-22 11:45:38 +0200

    repair demo

--- Day10_Forms/demo/demo1/main.js
+++ Day10_Forms/demo/demo1/main.js
@@ -1,20 +1,7 @@
-// function feeling() {
-//   let range = document.querySelector("input[type='range']").value;
-//   let output = document.querySelector(".output");
-//   output.innerHTML = `ich fühle mich wie ein ${range}`;
-// }
+console.log("Hallo Welt!");
 
-
-// Event listener, der ausgelöst wird, wenn sich der Wert des Bereichs ändert
-document.addEventListener("DOMContentLoaded", function () {
-  // Funktion, die den Wert des Bereichs in das .output-Element einfügt
-  function feeling() {
-    let range = document.querySelector("input[type='range']").value;
-    let output = document.querySelector(".output");
-    output.innerHTML = `Ich fühle mich wie eine ${range}`;
-  }
-
-  // Hinzufügen des Event Listeners zum "input" Element vom Typ "range"
-  let rangeInput = document.querySelector("input[type='range']");
-  rangeInput.addEventListener("input", feeling);
-});
+function feeling() {
+  let range = document.querySelector("input[type='range']").value;
+  let output = document.querySelector(".output");
+  output.innerHTML = `Ich fühle mich wie eine ${range}`;
+}
