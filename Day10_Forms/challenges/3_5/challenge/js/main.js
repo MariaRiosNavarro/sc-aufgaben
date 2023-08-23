de1ad73289a75c02748bc87d60462bc1d65fdd28
SuperCoder <propscode254@gmail.com>
2023-08-23 09:18:12 +0200

    chnage name

--- Day10_Forms/challenges/3_5/challenge/js/main.js
+++ Day10_Forms/challenges/3_5/challenge/js/main.js
@@ -1,4 +1,13 @@
+// console.log("Hallo Welt!");
+
+// function message() {
+
+//   let message = document.querySelector(".message");
+//   message.classList.add("show-message");
+// }
+
+
 function toggleMessage() {
-  let message = document.querySelector(".message");
-  message.classList.toggle("show-message");
-}
+    let message = document.querySelector(".message");
+    message.classList.toggle("show-message");
+  }
