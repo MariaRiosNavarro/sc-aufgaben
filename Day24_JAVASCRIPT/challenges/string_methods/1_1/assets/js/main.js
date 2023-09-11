e5b939b5cc478c1cafe1f65168c81334157dd7b5
SuperCoder <propscode254@gmail.com>
2023-09-11 14:48:35 +0200

    done 1_1

---
+++ Day24_JAVASCRIPT/challenges/string_methods/1_1/assets/js/main.js
@@ -0,0 +1,7 @@
+// introduction of .length The length property returns the length of a string.
+let firstName = "Maria";
+let lastName = "Rios Navarro";
+console.log("firstName length is", firstName.length);
+console.log("lastName length is", lastName.length);
+let fullName = firstName + lastName;
+console.log("fullName lenght is", fullName.length);
