6daa87302880dd4208927190f74d7d8c900a86d6
SuperCoder <propscode254@gmail.com>
2023-09-11 13:35:54 +0200

    finisch and extend 1_5

---
+++ Day24_JAVASCRIPT/challenges/variablen/1_6/assets/js/main.js
@@ -0,0 +1,27 @@
+const carName = "BMW";
+console.log(carName);
+console.log({ carName });
+const x = 150;
+console.log(x);
+console.log({ x });
+const y = 50;
+console.log(y);
+console.log({ y });
+const z = x + y;
+console.log(z);
+console.log({ z });
+
+let firstName = "John";
+console.log(firstName);
+console.log({ firstName });
+let lastName = "Doe";
+console.log(lastName);
+console.log({ lastName });
+let age = 35;
+console.log(age);
+console.log({ age });
+
+//concatenation
+console.log(firstName + " " + lastName + " is " + age + " years old");
+//string literal
+console.log(`${firstName} ${lastName} is ${age} years old`);
