d407eada3bfec318dac5bd0a58a103e76c098a2a
SuperCoder <propscode254@gmail.com>
2023-09-11 13:49:38 +0200

    finish 1_6

--- Day24_JAVASCRIPT/challenges/variablen/1_6/assets/js/main.js
+++ Day24_JAVASCRIPT/challenges/variablen/1_6/assets/js/main.js
@@ -1,27 +1,36 @@
-const carName = "BMW";
-console.log(carName);
-console.log({ carName });
-const x = 150;
-console.log(x);
+//Let vs const
+
+let x = 34;
+x = 67;
 console.log({ x });
-const y = 50;
-console.log(y);
-console.log({ y });
-const z = x + y;
-console.log(z);
-console.log({ z });
 
-let firstName = "John";
-console.log(firstName);
-console.log({ firstName });
-let lastName = "Doe";
-console.log(lastName);
-console.log({ lastName });
-let age = 35;
-console.log(age);
-console.log({ age });
+// Das let-Schlüsselwort wurde in ES6 (2015) eingeführt.
 
-//concatenation
-console.log(firstName + " " + lastName + " is " + age + " years old");
-//string literal
-console.log(`${firstName} ${lastName} is ${age} years old`);
+// Mit let definierte Variablen können nicht neu deklariert werden
+
+// Mit let definierte Variablen müssen vor der Verwendung deklariert werden
+
+// Mit let definierte Variablen haben Block-Scope
+
+const y = 34;
+y = 67; // Fehler: Variable kann nicht verändert werden,
+//  da sie mit const definiert ist!
+
+//Das Schlüsselwort wurde in ES6 (2015)const eingeführt.
+
+// Mit const definierte Variablen können nicht erneut deklariert  werden
+
+// Mit const definierte Variablen können nicht neu zugewiesen werden
+
+// Mit const definierte Variablen haben einen Blockbereich
+
+// !Wann sollte JavaScript const verwendet werden?
+
+// !Deklarieren Sie eine Variable immer mit const, wenn Sie wissen, dass der Wert nicht geändert werden sollte.
+
+// !Verwenden Sie es const, wenn Sie Folgendes deklarieren:
+
+// !Ein neues Array
+// !Ein neues Objekt
+// !Eine neue Funktion
+// !Ein neuer RegExp
