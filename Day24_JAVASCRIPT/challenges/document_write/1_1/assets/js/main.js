0496976a73d5f76fb3f009e2bc9d946d028b13c7
SuperCoder <propscode254@gmail.com>
2023-09-11 15:32:30 +0200

    done

---
+++ Day24_JAVASCRIPT/challenges/document_write/1_1/assets/js/main.js
@@ -0,0 +1,19 @@
+// Eine h1 mit “Hello World!”
+// Einen p mit “Have a nice day!”
+// Deklariere die Variable myText und fülle sie mit “Hello again”.
+//  Schreibe die Variable anschließend in dein Dokument.
+// Deklariere Variablen: vorname, nachname und fülle sie mit deinem Namen.
+// Schreibe sie anschließend in dein Dokument.
+
+document.write("<h1>Hello World!</h1>");
+document.write("<p>Have a nice day!</p>");
+let myText = "Hello again";
+document.write(myText);
+document.write("<br>");
+document.write("<hr>");
+let firstName = "Maria";
+let lastName = "Rios Navarro";
+document.write("My name is " + firstName + " " + lastName + ".");
+document.write("<hr>");
+// with string literal
+document.write(`I am ${firstName} ${lastName}`);
