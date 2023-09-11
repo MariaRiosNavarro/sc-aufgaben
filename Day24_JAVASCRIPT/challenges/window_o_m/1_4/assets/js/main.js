ca3494cca8622cbf0c48a7f96c307192f3550f9b
SuperCoder <propscode254@gmail.com>
2023-09-11 15:22:57 +0200

    done

---
+++ Day24_JAVASCRIPT/challenges/window_o_m/1_4/assets/js/main.js
@@ -0,0 +1,24 @@
+// window.alert("Hallo Welt");
+// window.prompt("Bitte geben Sie Ihren Namen ein.");
+// window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+// window.confirm("Stimmen Sie meiner Meinung zu?");
+
+let alter = window.prompt("Gib mir bitte dein Alter:");
+console.log(alter);
+let b = 5;
+let a = b * 5 - 3;
+console.log({ a });
+window.alert(a);
+let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+let grosse = "2,72m";
+window.confirm(mann + grosse);
+
+// Window alert() https://www.w3schools.com/jsref/met_win_alert.asp
+// (INPUT)!!-Window prompt() https://www.w3schools.com/jsref/met_win_prompt.asp
+// Window confirm() https://www.w3schools.com/jsref/met_win_confirm.asp
+
+// My test
+
+window.alert("take care");
+window.prompt("your name", "write your name");
+window.confirm("well done?");
