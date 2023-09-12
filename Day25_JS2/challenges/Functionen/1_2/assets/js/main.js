6f5eac05292d2b4349e910be3e5b5e8f4bfc2d9e
SuperCoder <propscode254@gmail.com>
2023-09-12 12:10:36 +0200

    add 3 version

--- Day25_JS2/challenges/Functionen/1_2/assets/js/main.js
+++ Day25_JS2/challenges/Functionen/1_2/assets/js/main.js
@@ -1,3 +1,5 @@
+// !version 1
+
 // function intro2(paramName) {
 //   let name = "Supercoder:in";
 //   console.log("Hi" + name + ". Mein Name ist " + paramName);
@@ -8,7 +10,7 @@
 // let resultSteffi = intro2("Steffi");
 // let resultJan = intro2("Jan");
 
-// let content = document.getElementById("result");
+// let content = document.getElementById("container");
 // content.innerHTML = "<h2>" + resultMaria + "</h2>";
 
 // Die Funktion intro2 gibt nichts explizit zurück,
@@ -18,23 +20,41 @@
 // die Funktion so ändern, dass sie einen Wert zurückgibt,
 // den wir dann in das HTML einfügen können.
 
-function intro2(paramName) {
-  let name = "Supercoder:in";
-  let result = "Hi " + name + ". Mein Name ist " + paramName;
-  console.log(result);
-  return result;
-}
+// !version2
+
+// function intro2(paramName) {
+//   let name = "Supercoder:in";
+//   let result = "Hi " + name + ". Mein Name ist " + paramName;
+//   console.log(result);
+//   return result;
+// }
 
 // Komentiere der console log und guck der ergebniss in der Preview/console
 // Das gleiche mit den return result
 
-let resultMaria = intro2("Maria");
-let resultMarzio = intro2("Marzio");
-let resultSteffi = intro2("Steffi");
-let resultJan = intro2("Jan");
+// let resultMaria = intro2("Maria");
+// let resultMarzio = intro2("Marzio");
+// let resultSteffi = intro2("Steffi");
+// let resultJan = intro2("Jan");
 
-let content = document.getElementById("result");
-content.innerHTML = "<h2>" + resultMaria + "</h2>";
-content.innerHTML += "<h2>" + resultMarzio + "</h2>";
-content.innerHTML += "<h2>" + resultSteffi + "</h2>";
-content.innerHTML += "<h2>" + resultJan + "</h2>";
+// let content = document.getElementById("container");
+// content.innerHTML = "<h2>" + resultMaria + "</h2>";
+// content.innerHTML += "<h2>" + resultMarzio + "</h2>";
+// content.innerHTML += "<h2>" + resultSteffi + "</h2>";
+// content.innerHTML += "<h2>" + resultJan + "</h2>";
+
+// !version 3
+
+let content = document.getElementById("container");
+
+function intro2(paramName) {
+  let name = "Supercoder:in";
+  let result = "Hi " + name + ". Mein Name ist " + paramName;
+  console.log(result);
+  content.innerHTML += "<h2>" + result + "</h2>";
+}
+
+intro2("Maria");
+intro2("Marzio");
+intro2("Steffi");
+intro2("Jan");
