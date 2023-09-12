7029aa408308755c1199f87937ba7a3a42367d57
SuperCoder <propscode254@gmail.com>
2023-09-12 11:40:31 +0200

    done 1_3

---
+++ Day25_JS2/challenges/innerHTML/1_3/assets/js/main.js
@@ -0,0 +1,3 @@
+let gallery = document.getElementById("gallery");
+gallery.innerHTML =
+  "<figure><img src='assets/img/cat.jpeg' width='200px' style='aspect-ratio: 1' /><p>Fig.1</p></figure><figure><img src='assets/img/cat.jpeg' width='200px' style='aspect-ratio: 1' /><p>Fig.2</p></figure><figure><img src='assets/img/cat.jpeg' width='200px' style='aspect-ratio: 1' /><p>Fig.3</p></figure>";
