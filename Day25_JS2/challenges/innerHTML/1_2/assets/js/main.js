a7ac28a6389c0d14230b0474cf6a5cbef84469b4
SuperCoder <propscode254@gmail.com>
2023-09-12 11:23:08 +0200

    done 1_2 innerHTML

---
+++ Day25_JS2/challenges/innerHTML/1_2/assets/js/main.js
@@ -0,0 +1,8 @@
+let mainContainer = document.getElementById("info");
+mainContainer.innerHTML = "<h1>Hello Wolrd!</h1>";
+mainContainer.innerHTML += "<h2>How are you?</h2>";
+
+let container = document.getElementById("container");
+container.innerHTML = "<p>start of the element</p>";
+
+document.write("end of the element");
