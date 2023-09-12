bf47d3f58efa96b8d09840b653fb1555d201b749
SuperCoder <propscode254@gmail.com>
2023-09-12 13:12:02 +0200

    done 1_4

---
+++ Day25_JS2/challenges/Functionen/1_4/assets/js/main.js
@@ -0,0 +1,20 @@
+let content = document.getElementById("contain");
+
+function hero(heroName, heroPower, heroEnemy) {
+  let name = `Mein:e Lieblingsheld:in ist: ${heroName}`;
+  let power = `Er/sie hat die Fähigkeit: ${heroPower}`;
+  let enemy = `Sein/ihr größte/r Gegner:in ist: ${heroEnemy}`;
+  let result = name + power + enemy;
+  console.log(result);
+  //   extra
+  content.innerHTML += `<h3 style="color:red">${result}</h3>`;
+}
+
+hero("Black Widow", "Spionage und Kampfkunst", "Taskmaster");
+hero("Captain Marvel", "Übermenschliche Kräfte und Flugfähigkeit", "Skrulls");
+hero("Scarlet Witch", "Hexenkräfte und Realitätsveränderung", "Thanos");
+hero(
+  "Superwoman",
+  "Übermenschliche Kräfte, Flugfähigkeit und Hitzeblick",
+  "Lex Luthor"
+);
