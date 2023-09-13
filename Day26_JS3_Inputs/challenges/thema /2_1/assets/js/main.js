c9d74bf05d7d6a196ed88a78f2c10dc5480a4304
SuperCoder <propscode254@gmail.com>
2023-09-13 15:03:50 +0200

    add coment

--- Day26_JS3_Inputs/challenges/thema /2_1/assets/js/main.js
+++ Day26_JS3_Inputs/challenges/thema /2_1/assets/js/main.js
@@ -1,4 +1,8 @@
 const headline = document.querySelector("#headline");
 
 const change = () => headline.classList.add("bck-red");
+// the same as:
+// function change(){
+//     headline.classList.add("bck-red");
+// }
 const reset = () => headline.classList.remove("bck-red");
