1ba11623589e0ffc7e12f7879c61ad8781ed3b0e
SuperCoder <propscode254@gmail.com>
2023-09-14 11:36:10 +0200

    now works

--- Day27_JS_conditionals/demo/mein/assets/js/main.js
+++ Day27_JS_conditionals/demo/mein/assets/js/main.js
@@ -1,58 +1,81 @@
-// function formValues() {
-//   const checkInput = document.querySelector(".check-input").checked;
-//   console.log(checkInput);
-//   const valueText = document.querySelector(".text-input").value;
-//   console.log(valueText);
-// }
+// inputsvalue
+// ohne submit funktioniert mit drucken de taste enter bei text input
 
-// const text = document.querySelector(".textHeadline");
+function formValues() {
+  const checkInput = document.querySelector(".check-input").checked;
+  console.log(checkInput);
+  const valueText = document.querySelector(".text-input").value;
+  console.log(valueText);
+}
 
-// function add() {
-//   text.classList.add("green");
-// }
+// classList
+const text = document.querySelector(".textHeadline");
 
-// function remove() {
-//   text.classList.remove("green");
-//   text.classList.remove("blue");
-// }
+function add() {
+  text.classList.add("green");
+}
 
-// function toggle() {
-//   text.classList.toggle("blue");
-// }
+function remove() {
+  text.classList.remove("green");
+  text.classList.remove("blue");
+}
+
+function toggle() {
+  text.classList.toggle("blue");
+}
 
 // !conditional if-else
 
 // if (bedienung) {wenn die bedienung wahr is}
 // else {wenn die bedienung false ist}
 
-// function checkNumber() {
-//   let num = Number(document.querySelector(".check-num").value);
-//   console.log({ num });
-//   let out = document.querySelector(".output-num");
+function checkNumber() {
+  let num = Number(document.querySelector(".check-num").value);
+  console.log({ num });
+  let out = document.querySelector(".output-num");
 
-//   if (num > 0) {
-//     out.innerHTML = "die zahl ist positiv";
-//   } else if (num === 0) {
-//     out.innerHTML = "die zahl ist 0";
-//   } else {
-//     out.innerHTML = "die zahl ist negativ";
-//   }
-// }
+  if (num > 0) {
+    out.innerHTML = "die zahl ist positiv";
+  } else if (num === 0) {
+    out.innerHTML = "die zahl ist 0";
+  } else {
+    out.innerHTML = "die zahl ist negativ";
+  }
+}
 
 // logiin
 
-const username1 = "maria12345";
-const password1 = "98765";
+const username2 = "maria12345";
+const password2 = "98765";
 
-function loginUser() {
-  let nameUser = document.querySelector("#name-user").value;
-  let pass2 = document.querySelector("#pass").value;
+function userCheck() {
+  let nameUserForm = document.querySelector("#name-user2").value;
+  let passUserForm = document.querySelector("#pass2").value;
+  const outputLogin2 = document.querySelector(".mess2");
+  // console.log({ nameUserForm }, { passUserForm });
 
-  let mess2 = document.querySelector("#mess");
-  console.log({ name2 }, { pass2 });
-  if (nameUser === username1 && pass2 === password1) {
-    mess2.innerHTML = "yes";
+  if (username2 === nameUserForm && password2 === passUserForm) {
+    outputLogin2.innerHTML = "yes";
   } else {
-    mess2.innerHTML = "no";
+    outputLogin2.innerHTML = "no";
+  }
+}
+// ----version
+const username = "mazio";
+const password = "123";
+
+function checkUserData() {
+  let loginUserName = document.querySelector("#username").value;
+  let loginUserPassword = document.querySelector("#password").value;
+  const outputLogin = document.querySelector(".login-output");
+
+  // Checkt ob die daten ankommen.
+  console.log({ loginUserName }, { loginUserPassword });
+
+  if (username === loginUserName && password === loginUserPassword) {
+    outputLogin.innerHTML = "<p class='green'> Login war erfolgreich. </p>";
+    window.location.replace("./secret.html");
+  } else {
+    outputLogin.innerHTML = "<p class='red'> Login war NICHT erfolgreich, </p>";
   }
 }
