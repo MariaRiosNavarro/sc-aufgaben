d211f0cb48f5d0482f5813f5e82d371ea0757ec5
SuperCoder <propscode254@gmail.com>
2023-09-14 13:28:46 +0200

    done 1_3

---
+++ Day27_JS_conditionals/challenges/thema /1_3/assets/js/main.js
@@ -0,0 +1,21 @@
+let legalAge = 18;
+
+function greaterThan() {
+  const message = document.querySelector(".output");
+  const age = Number(document.getElementById("input").value);
+
+  //   if (age >= legalAge) {
+  //     message.innerHTML = "<p class='green'>Ja, Du kannst Shisha rauchen 💨</p>";
+  //   } else {
+  //     message.innerHTML =
+  //       "<p class='red' >Du darfst noch nicht Shisha rauchen 🎡 </p>";
+  //   }
+
+  // !Version with ternary operator:
+
+  age >= legalAge
+    ? (message.innerHTML =
+        "<p class='green'>Ja, Du kannst Shisha rauchen 💨</p>")
+    : (message.innerHTML =
+        "<p class='red' >Du darfst noch nicht Shisha rauchen 🎡 </p>");
+}
