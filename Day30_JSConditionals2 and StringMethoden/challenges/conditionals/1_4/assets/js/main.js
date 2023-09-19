fe9643291e613235af21b77d1bcffa80330542cd
SuperCoder <propscode254@gmail.com>
2023-09-19 16:04:00 +0200

    add

--- Day30_JSConditionals2 and StringMethoden/challenges/conditionals/1_4/assets/js/main.js
+++ Day30_JSConditionals2 and StringMethoden/challenges/conditionals/1_4/assets/js/main.js
@@ -9,11 +9,23 @@ console.log(outputText);
 function passwordCheck() {
   let value = input.value;
 
-  if (value.length < 8) {
-    outputText.innerHTML = "Password is too short";
-    // outputText.sytle.color = "red";
-  } else {
-    outputText.innerHTML = "Welcome to your account";
-    // outputText.sytle.color = "green";
-  }
+  let valid = '<span style="color:red;">Password is too short</span>';
+  let invalid = '<span style="color:green;">Welcome to your account</span>';
+
+  //   if (value.length < 8) {
+  //     outputText.innerHTML = "Password is too short";
+  //     outputText.style.color = "red";
+  //   } else {
+  //     outputText.innerHTML = "Welcome to your account";
+  //     outputText.style.color = "green";
+  //   }
+
+  value.length < 8
+    ? (outputText.innerHTML = valid)
+    : (outputText.innerHTML = invalid);
 }
+
+// remove everything in the input when we clik another time to give a new password
+input.addEventListener("mouseover", function () {
+  input.value = "";
+});
