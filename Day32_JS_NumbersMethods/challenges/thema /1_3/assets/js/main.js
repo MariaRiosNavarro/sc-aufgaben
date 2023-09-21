6add6cc207d3ef8494494be2be742f170f174ccf
SuperCoder <propscode254@gmail.com>
2023-09-21 10:50:42 +0200

    done 1_3

---
+++ Day32_JS_NumbersMethods/challenges/thema /1_3/assets/js/main.js
@@ -0,0 +1,19 @@
+// In dieser Übung geht es um Math.random() und Math.floor().
+// Deklariere die Variable randomNum
+
+let randomNum = Math.random();
+console.log(randomNum);
+
+// und und gebe darin eine zufällige Nummer aus.
+// Deklariere die Variable randomNum1_10
+
+let randomNum1_10 = Math.round(randomNum * 10);
+console.log(randomNum1_10);
+
+// und gebe darin eine zufällige Nummer zwischen 1 und 10 aus.
+// Deklariere die Variable randomNum1_100
+
+let randomNum1_100 = Math.round(randomNum * 100);
+console.log(randomNum1_100);
+
+// und gebe darin eine zufällige Nummer zwischen 1 und 100 aus.
