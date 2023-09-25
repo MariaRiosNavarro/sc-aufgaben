ef42a45518f73157e84d9ce2cb9dec74de84a8a0
SuperCoder <propscode254@gmail.com>
2023-09-25 12:30:09 +0200

    done 1_10

--- Day34_ArrayMethods/challenges/thema /1_(1,2)/assets/js/main.js
+++ Day34_ArrayMethods/challenges/thema /1_(1,2)/assets/js/main.js
@@ -105,3 +105,19 @@ console.log({ meals });
 meals.unshift(firstOneMeals, firstSecondMeals);
 console.log(meals);
 console.log({ meals });
+
+// 1_10
+
+console.log("-----1_10-------");
+
+const text =
+  "It's okay, Bender. I like cooking too. Oh, you're a dollar naughtier than most. My fellow Earthicans, as I have explained in my book 'Earth in the Balance', and the much more popular 'Harry Potter and the Balance of Earth', we need to defend our planet against pollution. Also dark wizards.";
+
+let pointSeparationArray = text.split(".");
+console.log(pointSeparationArray);
+
+let spaceSeparationArray = text.split(" ");
+console.log(spaceSeparationArray);
+
+let allCharacterSeparation = text.split("");
+console.log(allCharacterSeparation);
