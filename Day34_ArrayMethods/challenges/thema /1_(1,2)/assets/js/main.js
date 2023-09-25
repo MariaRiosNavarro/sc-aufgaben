d5e225e8c053eea110e06db7dd372569f75b2812
SuperCoder <propscode254@gmail.com>
2023-09-25 11:51:19 +0200

    done 1_2

---
+++ Day34_ArrayMethods/challenges/thema /1_(1,2)/assets/js/main.js
@@ -0,0 +1,17 @@
+const person = ["Jane", "Doe", "Mustername"];
+const friends = ["Joe", "Jade", "John"];
+const meals = ["lasagne", "burger", "flan"];
+
+console.log({ person, friends, meals });
+
+console.log(person[0]);
+console.log(person[1]);
+console.log(person[2]);
+
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+
+console.log(meals[0]);
+console.log(meals[1]);
+console.log(meals[2]);
