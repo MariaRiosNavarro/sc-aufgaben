483c527eb3272a0150011ab1a3e4f7f97a8c50be
SuperCoder <propscode254@gmail.com>
2023-09-26 14:06:20 +0200

    1_3 with extra

--- Day35_ArrayMethode2/challenges/forEach&map/all/assets/js/main.js
+++ Day35_ArrayMethode2/challenges/forEach&map/all/assets/js/main.js
@@ -22,3 +22,37 @@ output.forEach((drink) => document.write(drink, "<br>"));
 
 let upperDrinks = copyGetranke.map((getraenke) => getraenke.toUpperCase());
 console.log(upperDrinks);
+
+// 1_3
+
+let array = [
+  18, 16, 80, 51, 47, 38, 95, 42, 68, 61, 34, 51, 20, 17, 56, 31, 100, 6, 5, 30,
+  74, 97, 28, 99, 91, 27, 73, 12, 92, 6, 27, 71, 26, 15, 78,
+];
+
+let copyArray = array.slice();
+
+let sortedArray = copyArray.sort((a, b) => a - b);
+
+console.log(sortedArray);
+
+let newArray = sortedArray.map((a) => a * 2);
+console.log(newArray);
+
+// to remove copys
+
+let noDuplicates = new Set(newArray);
+
+console.log(noDuplicates);
+
+// or with for each
+
+let noDuplicates2 = [];
+
+newArray.forEach((element) => {
+  if (!noDuplicates2.includes(element)) {
+    noDuplicates2.push(element);
+  }
+});
+
+console.log(noDuplicates2);
