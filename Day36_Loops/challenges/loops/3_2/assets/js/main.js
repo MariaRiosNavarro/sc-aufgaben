286bd05d1df95ebcbbe61617102f7db1680093fd
SuperCoder <propscode254@gmail.com>
2023-09-27 17:13:02 +0200

    done

---
+++ Day36_Loops/challenges/loops/3_2/assets/js/main.js
@@ -0,0 +1,27 @@
+let numArr = [5, 22, 15, 100, 55];
+
+const out = document.querySelector('[data-js="output"]');
+
+let divisor = 1;
+
+const searchModulo = (num) => {
+  for (let i = 1; i <= 100; i++) {
+    divisor = i;
+    if (
+      num % divisor === 0 &&
+      divisor <= num &&
+      divisor !== num &&
+      divisor !== 1
+    ) {
+      let result = num / divisor;
+      console.log(
+        `${num} is devidable by ${divisor} . The result is ${result}`
+      );
+      out.innerHTML += `${num} is devidable by ${divisor} . The result is ${result} <br>`;
+    }
+  }
+};
+
+for (let j = 0; j < numArr.length; j++) {
+  searchModulo(numArr[j]);
+}
