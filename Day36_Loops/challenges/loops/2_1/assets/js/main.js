b57f49f9ab8100ec362391697e28460e892ff4f4
SuperCoder <propscode254@gmail.com>
2023-09-27 12:40:18 +0200

    change 2_1

--- Day36_Loops/challenges/loops/2_1/assets/js/main.js
+++ Day36_Loops/challenges/loops/2_1/assets/js/main.js
@@ -1,12 +1,15 @@
-const imageArray = [];
-
-for (let i = 1; i < 101; i++) {
-  if (i < 10) {
-    imageArray.push(`image_00${i}.jpg`);
-  } else if (i > 9 && i < 99) {
-    imageArray.push(`image_0${i}.jpg`);
-  } else {
-    imageArray.push(`image_${i}.jpg`);
+const returnArray = () => {
+  const imageArray = [];
+  for (let i = 1; i < 101; i++) {
+    if (i < 10) {
+      imageArray.push(`image_00${i}.jpg`);
+    } else if (i > 9 && i < 99) {
+      imageArray.push(`image_0${i}.jpg`);
+    } else {
+      imageArray.push(`image_${i}.jpg`);
+    }
   }
-}
-console.log(imageArray);
+  return console.log(imageArray);
+};
+
+returnArray();
