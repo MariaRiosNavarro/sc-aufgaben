7ede1dc405b4330adbf203175b732e89a7aed35d
SuperCoder <propscode254@gmail.com>
2023-09-27 11:56:08 +0200

    done 1_2

--- Day36_Loops/challenges/loops/1_1 bis 1_4/assets/js/main.js
+++ Day36_Loops/challenges/loops/1_1 bis 1_4/assets/js/main.js
@@ -1,7 +1,28 @@
 // 1_1
-
+console.log(
+  "%c-------------------Aufgabe--1_1---------------------",
+  "background-color: green; color: black;"
+);
 for (let i = 0; i < 10; i++) {
   console.log(i + 1, "Hello World"); //füge die i+1 vorher um die ausgabe in terminal getreent zu sehen und die zu numerieren length = index+1;
 }
 
 // 1_2
+console.log(
+  "%c-------------------Aufgabe--1_2---------------------",
+  "background-color: green; color: black;"
+);
+let numArray = [];
+
+for (let i = 0; i < 11; i++) {
+  numArray.push(i);
+  console.log("inside loop", numArray);
+}
+
+console.log("outside the loop", numArray);
+
+// 1_3
+console.log(
+  "%c-------------------Aufgabe--1_3---------------------",
+  "background-color: green; color: black;"
+);
