ea4ff650e9b6b62874c4974f7f3d428fef17153f
SuperCoder <propscode254@gmail.com>
2023-09-27 11:50:05 +0200

    done 1_1

---
+++ Day36_Loops/challenges/loops/1_1 bis 1_4/assets/js/main.js
@@ -0,0 +1,7 @@
+// 1_1
+
+for (let i = 0; i < 10; i++) {
+  console.log(i + 1, "Hello World"); //füge die i+1 vorher um die ausgabe in terminal getreent zu sehen und die zu numerieren length = index+1;
+}
+
+// 1_2
