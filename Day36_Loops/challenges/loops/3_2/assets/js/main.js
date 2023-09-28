1a9a90a195f2d9241fdc7d719d3a18d36a5b73e4
SuperCoder <propscode254@gmail.com>
2023-09-28 09:32:02 +0200

    add while

--- Day36_Loops/challenges/loops/3_2/assets/js/main.js
+++ Day36_Loops/challenges/loops/3_2/assets/js/main.js
@@ -4,24 +4,61 @@ const out = document.querySelector('[data-js="output"]');
 
 let divisor = 1;
 
-const searchDivider = (num) => {
-  for (let i = 1; i <= 100; i++) {
-    divisor = i;
-    if (
-      num % divisor === 0 &&
-      divisor <= num &&
-      divisor !== num &&
-      divisor !== 1
-    ) {
-      let result = num / divisor;
-      console.log(
-        `${num} is devidable by ${divisor} . The result is ${result}`
-      );
-      out.innerHTML += `${num} is devidable by ${divisor} . The result is ${result} <br>`;
+// const searchDivider = (num) => {
+//   for (let i = 1; i <= 100; i++) {
+//     divisor = i;
+//     if (
+//       num % divisor === 0 &&
+//       divisor < num &&
+//       divisor !== num &&
+//       divisor !== 1
+//     ) {
+//       let result = num / divisor;
+//       console.log(
+//         `${num} is devidable by ${divisor} . The result is ${result}`
+//       );
+//       out.innerHTML += `${num} is devidable by ${divisor} . The result is ${result} <br>`;
+//     }
+//   }
+// };
+
+// for (let j = 0; j < numArr.length; j++) {
+//   searchDivider(numArr[j]);
+// }
+
+// version for of
+
+const searchDivider = () => {
+  for (const element of numArr) {
+    // einzelne werte von array
+    for (let i = 0; i <= element; i++) {
+      if (element % i === 0 && i !== 1 && i !== element) {
+        console.log(i);
+        console.log(
+          ` ${element} ist telibar durch ${i}. Ergebnis ist ${element / i}`
+        );
+        out.innerHTML += `${element} is devidable by ${i} . The result is ${
+          element / i
+        } <br>`;
+      }
     }
   }
 };
 
-for (let j = 0; j < numArr.length; j++) {
-  searchDivider(numArr[j]);
-}
+searchDivider();
+
+// version mit continue (um werte zu überspringen)
+
+// let numArr = [5, 22, 15, 100, 55];
+
+// for (let i = 0; i < numArr.length; i++) {
+//   for (let j = 0; j < numArr[i]; j++) {
+//     if (j === 1) {
+//       continue;// wenn mit sich selbst teilbar ist, brauchen wir den wert nicht
+//     }
+
+//     if (numArr[i] % j === 0) {
+//       console.log(`${numArr[i]} is devidable by ${j}. The result is: ${numArr[i] / j}`);
+//     }
+//   }
+// }
---
+++ Day37_Loops-While_DoWhile/demo/demo1/assets/js/main.js
@@ -0,0 +1,19 @@
+// do while & while
+
+// do while - simple sintaxEr gibt immer mindestens ein mal egal dass der bedienung nicht trift,
+// zb let coins = 7, wird 8 ausgeben und dann abrechen
+
+let coins = 0;
+
+do {
+  coins++;
+  console.log(coins);
+} while (coins < 5);
+
+// while
+// simple syntax
+
+while (coins < 5) {
+  coins++;
+  console.log(coins);
+}
