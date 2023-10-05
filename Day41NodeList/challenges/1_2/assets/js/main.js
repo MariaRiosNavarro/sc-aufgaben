00812d106278687b25eef196976024abf7d40146
SuperCoder <propscode254@gmail.com>
2023-10-05 14:31:26 +0200

    1_2 and rename all

---
+++ Day41NodeList/challenges/1_2/assets/js/main.js
@@ -0,0 +1,14 @@
+let div = document.children[0].children[1].children[0];
+
+console.log(div);
+
+let divChildrem = div.children;
+
+console.log("children", divChildrem);
+
+const myFunction = () => {
+  for (let i = 0; i < div.children.length; i++) {
+    let child = div.children[i];
+    child.classList.toggle("red");
+  }
+};
