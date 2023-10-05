2b11c6067e674157fce41d5438efb6aab450e8d0
SuperCoder <propscode254@gmail.com>
2023-10-05 17:15:39 +0200

    fix

--- Day41NodeList/challenges/2_8/assets/js/main.js
+++ Day41NodeList/challenges/2_8/assets/js/main.js
@@ -1,8 +1,8 @@
 const list = document.body.querySelector("ul");
 console.log(list);
 
-let input = document.body.querySelector("input[type='text']");
-let button = document.body.querySelector("input[type='submit']");
+const input = document.body.querySelector("input[type='text']");
+const button = document.body.querySelector("input[type='submit']");
 
 button.addEventListener("click", () => {
   const newItem = document.createElement("li");
