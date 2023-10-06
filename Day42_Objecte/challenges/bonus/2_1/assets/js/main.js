270589bb7648b8c82d8bcb6aa76ca3517e00c936
SuperCoder <propscode254@gmail.com>
2023-10-06 16:30:04 +0200

    add elisas demo als coment

--- Day42_Objecte/challenges/bonus/2_1/assets/js/main.js
+++ Day42_Objecte/challenges/bonus/2_1/assets/js/main.js
@@ -14,6 +14,8 @@ let metalNameArray = [];
 let preiseArray = [];
 let changeArray = [];
 
+// Alle arrays af einmal erstellen
+
 edelMetallPreise.forEach((metal) => {
   let metalName = metal.name;
   metalNameArray.push(metalName);
@@ -73,13 +75,10 @@ titles.map((title) => {
   table.appendChild(th);
   th.style.outline = "3px solid gray";
   th.style.padding = "1rem";
-  //   th.style.margin = "3px";
   th.textContent = title;
 });
 
 // ----------- Zellen---
-
-console.log("test-------------");
 
 let row = edelMetallPreise.forEach((metal) => {
   // --- Lang Version
@@ -101,12 +100,11 @@ let row = edelMetallPreise.forEach((metal) => {
   //   thPreis.textContent = preis;
   //   thChange.textContent = change;
   // --- Compact Version
-  let tr = document.createElement("tr");
-  table.appendChild(tr);
+  let tr = document.createElement("tr"); //row erstellen
+  table.appendChild(tr); //zum  table hintufügen
   for (const key in metal) {
-    // jede value des Objects
+    // von jede value des Objects
     const value = metal[key];
-    console.log(value);
     // wir  machen eine zelle
     let td = document.createElement("td");
     // wir fügen diese zelle an den row von oben
@@ -117,3 +115,97 @@ let row = edelMetallPreise.forEach((metal) => {
     td.style.border = "1px solid gray";
   }
 });
+
+// #ELISAS VERSION:
+
+// Erstelle ein Array aller Namen der Edelmetalle
+
+// const allNames = [];
+// edelMetallPreise.map(elt => allNames.push(elt.name));
+
+// Erstelle ein Array aller Preise in Euro pro Gramm
+// const allPreise = [];
+// edelMetallPreise.map(elt => allPreise.push(elt.preiseGramEuro));
+
+//Preise filtern
+// console.log(
+// 	"Edelmetalle über 50 Euro:",
+// 	allPreise.filter(elt => elt > 50),
+// );
+
+// Erstelle ein Array aller Veraenderungen
+// const allVeraenderungen = [];
+// edelMetallPreise.map(elt => allVeraenderungen.push(elt.veraenderung));
+
+// Ziehe die Schlüssel (Keys) aus dem ersten Objekt
+// const keys = Object.keys(edelMetallPreise[0]);
+// console.log("Keys:", keys);
+
+// Erstelle eine HTML-Tabelle mit den Schlüsseln als Spaltenüberschriften
+// const table = `<table>
+//     <tr>
+//         <th>${keys[0]}</th>
+//         <th>${keys[1]}</th>
+//         <th>${keys[2]}</th>
+//     </tr>
+// </table>`;
+
+// Füge die Tabelle in den Body des Dokuments ein
+// document.body.insertAdjacentHTML("afterbegin", table);
+
+// Iteriere über die Edelmetall-Objekte und füge sie als Zeilen in die Tabelle ein
+// edelMetallPreise.forEach(elt => {
+// 	const tablerow = `<tr>
+//         <td>${elt.name}</td>
+//         <td>${elt.preiseGramEuro}</td>
+//         <td>${elt.veraenderung}</td>
+//     </tr>`;
+
+// Hier füge ich die Zeile zur Tabelle hinzu
+// 	document.body.children[0].children[0].insertAdjacentHTML(
+// 		"beforeend",
+// 		tablerow,
+// 	);
+// });
+
+//! WICHTIGER HINWEIS :
+// - In meinem Beispiel habe ich `insertAdjacentHTML`
+// anstelle von `appendChild` verwendet, um HTML-Inhalte
+// zu einem `table`-Element hinzuzufügen. Dies liegt daran,
+// dass `insertAdjacentHTML` dazu dient, HTML-Text in ein
+// DOM-Element einzufügen, während `appendChild` verwendet
+// wird, um ein vorhandenes DOM-Element als Kind zu einem
+// anderen DOM-Element hinzuzufügen. Es gibt einen
+// entscheidenden Unterschied zwischen diesen beiden Methoden:
+
+// - `insertAdjacentHTML`: Diese Methode erwartet eine
+// Zeichenfolge mit HTML-Tags und fügt diese Tags in das
+// DOM-Element ein. Sie ermöglicht die Einfügung von
+// HTML-Text an verschiedenen Stellen innerhalb des
+// Elements, wie "beforebegin", "afterbegin", "beforeend"
+// und "afterend". In meinem Beispiel habe ich "beforeend"
+// verwendet, um die `tablerow` am Ende der Tabelle hinzuzufügen.
+
+// - `appendChild`: Diese Methode erwartet ein
+// vorhandenes DOM-Element und fügt dieses Element als
+// Kind zu einem anderen DOM-Element hinzu. Normalerweise
+// wird sie verwendet, um DOM-Elemente hinzuzufügen,
+//  nicht jedoch HTML-Text.
+
+// - Da ich HTML-Text (die `tablerow`)
+// in ein `table`-Element einfüge,
+// ist `insertAdjacentHTML` die geeignete Methode dafür.
+// Wenn ich stattdessen `appendChild` verwendet hätte,
+// müsste ich zuerst DOM-Elemente für `table`,
+// `tr` und `td` erstellen und dann die
+// entsprechenden Inhalte zu diesen DOM-Elementen
+// hinzufügen,bevor ich sie mit `appendChild`
+//  der Tabelle hinzufüge.
+//!Komentar von mir, ich habe mit AppendChild gemacht und konnte ohne Inhalt hinzufügen?!,
+// !DIE TABELLE  IST AM ENDE SICHBAR??!
+
+// - In meinem Fall bietet `insertAdjacentHTML`
+// eine einfachere Möglichkeit, den HTML-Text
+// (die `tablerow`) der Tabelle hinzuzufügen,
+//  ohne zusätzliche DOM-Elemente manuell
+//  erstellen zu müssen.
