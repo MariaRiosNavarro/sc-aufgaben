63c593a1d4a7d2d07f9c1621a42fb2ee749d1900
SuperCoder <propscode254@gmail.com>
2023-10-06 12:38:47 +0200

    done with bonus

---
+++ Day42_Objecte/challenges/objecte/1_7/assets/js/main.js
@@ -0,0 +1,37 @@
+// "artist", "title", "medium", Gib alle “release_year” älter als 1975 aus.
+
+let myMusic = [
+  {
+    artist: "The Beatles",
+    title: "Abbey Road",
+    release_year: 1969,
+    medium: ["LP", "CD", "MC", "Download"],
+    gold: true,
+  },
+  {
+    artist: "Pink Floyd",
+    title: "Dark Side of the Moon",
+    release_year: 1978,
+    medium: ["CS", "CD", "LP", "Download"],
+    gold: true,
+  },
+  {
+    artist: "Led Zeppelin",
+    title: "Led Zeppelin IV",
+    release_year: 1971,
+    medium: ["CS", "LP", "Download"],
+    gold: true,
+  },
+  {
+    artist: "Metallica",
+    title: "Kill ’Em All und Ride the Lightning",
+    release_year: 1983,
+    medium: ["LP", "CD", "MC", "Download"],
+    gold: true,
+  },
+];
+
+myMusic.forEach((album) => {
+  console.log("1", album.artist, "2", album.title, "3", album.medium);
+  album.release_year > 1975 ? console.log(">1975", album.release_year) : "";
+});
