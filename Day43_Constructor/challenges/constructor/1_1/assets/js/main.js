a5e7c5ace6e3e5ac3f5f976fc6d2d00cf0f62ccc
SuperCoder <propscode254@gmail.com>
2023-10-09 11:57:04 +0200

    done

--- Day43_Constructor/challenges/constructor/1_1/assets/js/main.js
+++ Day43_Constructor/challenges/constructor/1_1/assets/js/main.js
@@ -1,7 +1,16 @@
 class Ball {
-    constructor(ballType){
-        this.ballType = ballType;
-    }
+  constructor(ballType) {
+    this.setBalltype(ballType);
+  }
+
+  setBalltype(newType) {
+    newType === undefined ? (newType = "regular") : (newType = newType);
+    this.ballType = newType;
+  }
 }
 
-const 
+const ball1 = new Ball();
+const ball2 = new Ball("super");
+
+console.log(ball1);
+console.log(ball2);
