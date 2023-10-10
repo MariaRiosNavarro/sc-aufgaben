e0177d74172bcb31598b31e7fac5285eee32ace1
SuperCoder <propscode254@gmail.com>
2023-10-10 12:44:41 +0200

    done

--- Day44_Methods/challenges/date/1_3/assets/js/main.js
+++ Day44_Methods/challenges/date/1_3/assets/js/main.js
@@ -1,4 +1,4 @@
-const date = new Date();
+let date = new Date();
 console.log(date); //Tue Oct 10 2023 12:14:42 GMT+0200 (Mitteleuropäische Sommerzeit)
 
 let year2222 = date.setFullYear(2222);
@@ -20,3 +20,20 @@ let nowMonth = date.setMonth(9);
 let nowYear = date.setFullYear(2023);
 
 console.log(date); //Tue Oct 10 2023 12:27:11 GMT+0200 (Mitteleuropäische Sommerzeit)
+
+let newDate30dayslater = new Date(
+  new Date(date).setDate(new Date(date).getDate() + 30)
+);
+let newDate10daysbefore = new Date(
+  new Date(date).setDate(new Date(date).getDate() - 10)
+);
+
+// console.log(newDate30dayslater); //Thu Nov 09 2023 12:37:29 GMT+0100 (Mitteleuropäische Normalzeit)
+
+// console.log(newDate10daysbefore); //Sat Sep 30 2023 12:37:18 GMT+0200 (Mitteleuropäische Sommerzeit)
+
+console.log(date); //Tue Oct 10 2023 12:37:47 GMT+0200 (Mitteleuropäische Sommerzeit)
+
+date = newDate30dayslater;
+
+console.log(date); //Thu Nov 09 2023 12:44:22 GMT+0100 (Mitteleuropäische Normalzeit)
