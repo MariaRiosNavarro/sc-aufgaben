615ab3e22f2a275dffd408aaaeb49c0f295bc694
SuperCoder <propscode254@gmail.com>
2023-10-10 13:11:19 +0200

    save

--- Day44_Methods/challenges/date/1_6/assets/js/main.js
+++ Day44_Methods/challenges/date/1_6/assets/js/main.js
@@ -3,12 +3,12 @@ const askAMPM = (date) => {
   hours < 12 ? console.log("AM") : console.log("PM");
 };
 
-let date1 = new Date(1999, 10, 5, 15); //PM
+let date1 = new Date(1999, 10, 5, 15);
 let date2 = new Date();
-let date3 = new Date(2019, 12, 3, 12); //PM
-let date4 = new Date(2000, 1, 1, 11); //AM
+let date3 = new Date(2019, 12, 3, 12);
+let date4 = new Date(2000, 1, 1, 11);
 
-askAMPM(date1);
-askAMPM(date2);
-askAMPM(date3);
-askAMPM(date4);
+askAMPM(date1); //PM
+askAMPM(date2); //PM
+askAMPM(date3); //PM
+askAMPM(date4); //AM
