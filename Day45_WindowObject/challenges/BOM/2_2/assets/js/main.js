ebcfe779f4ad9798ca6e1f1d9e8e4aed87811ee7
SuperCoder <propscode254@gmail.com>
2023-10-11 16:39:51 +0200

    add coments

--- Day45_WindowObject/challenges/BOM/2_2/assets/js/main.js
+++ Day45_WindowObject/challenges/BOM/2_2/assets/js/main.js
@@ -7,23 +7,24 @@ const date = new Date(1970, 1); // date fängt schon von 0 min 0 sec 0 milisec
 
 let minutes = 0;
 
-// Nehmen den Value, wichtig return date, um bei der updateTimer zu nutzen
+//# 1-Nehmen den Value, wichtig return date, um bei der updateTimer zu nutzen
 
 minutesInput.addEventListener("input", () => {
   minutes = minutesInput.value;
   date.setMinutes(minutes);
   const outSeconds = date.getSeconds();
+  //   0 hinzufügen bei der sekunden, wenn der nummer < 10 ist
   timeOutput.innerHTML = `${date.getMinutes()}:${
     outSeconds < 10 ? `0${outSeconds}` : outSeconds
   }`;
   return minutes, date;
 });
 
-// Value wird für den Timer zu updaten - 1
+//#2- Update Timer immer - 1 secunde, bis 00:00
 
 const updateTimer = () => {
   const outSeconds = date.getSeconds();
-  //   0 hinzufügen bei der sekunden, wenn der nummer < 10 ist
+  //   0 hinzufügen bei der sekunden, wenn der nummer < 10 ist, so wie oben
   timeOutput.innerHTML = `${date.getMinutes()}:${
     outSeconds < 10 ? `0${outSeconds}` : outSeconds
   }`;
@@ -35,8 +36,8 @@ const updateTimer = () => {
   }
 };
 
-// Interval startMinCountdown & restartMinCountdown sind die gleiche funktion
-// wichtig return interval, damit wir danach enfernen können
+// #3 - Interval startMinCountdown & restartMinCountdown sind die gleiche funktion
+// # wichtig return interval, damit wir danach enfernen können
 
 let interval;
 
